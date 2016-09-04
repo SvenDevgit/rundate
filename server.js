91d83aaf400cfa5c8e036c935876f51f381cf8bc
SvenDevgit <svenlambeck@hotmail.com>
2016-09-05 00:08:54 +0200

    third Commit

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ var app = express();
 
 app.use(express.static('public'));
 
+app.get('/', function(req, res) {
+ res.json('hallo world server');
 
-
-
-
+});
 
 app.listen(process.env.PORT || 8080);
+
+exports.app = app;
